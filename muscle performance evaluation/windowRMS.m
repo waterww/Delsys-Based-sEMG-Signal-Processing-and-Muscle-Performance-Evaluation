75ced1c142136f7c0a6d67061e5e5916052ab34f
waterww <45504345+waterww@users.noreply.github.com>
2019-03-20 00:33:29 +0800

    Rename windowRMS.m to muscle performance evaluation/windowRMS.m

--- muscle performance evaluation/windowRMS.m
+++ muscle performance evaluation/windowRMS.m
@@ -1,8 +1,8 @@
 function [RS] = windowRMS(data)
-%�󼡵��źŵľ�����ֵ�����ڴ�СΪ50���غ�40
-%   �˴���ʾ��ϸ˵��
+%求肌电信号的均方根值，窗口大小为50，重合40
+%   此处显示详细说明
 n=(length(data)-50)/10+1;
-n=floor(n);%���ڵ�����
+n=floor(n);%窗口的数量
 RS=zeros(n,1);
 for i=1:n
     data_window=data((10*(i-1)+1):(10*(i-1)+50));
